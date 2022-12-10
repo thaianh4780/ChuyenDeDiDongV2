e0eb6492de409f830dce3a64e22b37a874843b82
thaianh4780 <100453973+thaianh4780@users.noreply.github.com>
2022-12-10 12:23:37 +0700

    fixing Androi

--- components/styles.js
+++ components/styles.js
@@ -73,7 +73,7 @@ export const FormLog = styled.View`
 
 export const StyledInputLabel = styled.Text`
   color: ${tertiary};
-  font-size: 18%;
+  font-size: 18px;
   text-align: left;
   font-weight: bold;
   padding-left: 2%;
@@ -83,7 +83,7 @@ export const StyledTextInput = styled.TextInput`
   background: ${secondary};
   padding-left: 20%;
   border-radius: 5px;
-  font-size: 16%;
+  font-size: 16px;
   height: 60px;
   color: ${tertiary};
   margin-vertical: 3%;
@@ -102,7 +102,7 @@ export const LeftIcon = styled.View`
 `;
 
 export const PageTitle = styled.Text`
-  font-size: 35%;
+  font-size: 35px;
   text-align: center;
   font-weight: bold;
   color: ${brand};
@@ -122,7 +122,7 @@ export const StyledButton = styled.TouchableOpacity`
 
 export const ButtonText = styled.Text`
   color: ${primary};
-  font-size: 20%;
+  font-size: 20px;
   font-weight: bold;
 `;
 
@@ -520,3 +520,10 @@ export const StyledHomeBrownLogo = styled.Image`
   bottom: 15%;
   left: 5%;
   `
+  export const TouchableHomeLogo = styled.TouchableOpacity`
+  height:50px;
+  width:50px;
+  border-radius: 100px;
+  margin-horizontal:45%; 
+  margin-vertical:2%;
+`
